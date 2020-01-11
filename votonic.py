be57bf1e7a9ec9691598289fd9363a5e83436d01
Tim Weber <scy@scy.name>
2020-01-11 06:35:15 +0100

    Parse some packets that we already understand

--- votonic.py
+++ votonic.py
@@ -1,6 +1,78 @@
 import serial
 import time
 
+class Packet:
+
+    def __init__(self, frame):
+        self.frame = frame
+
+    def __str__(self):
+        return " ".join("{:02x}".format(byte) for byte in self.frame)
+
+    def toSignedInt(self, raw, decimals=0):
+        val = round(int.from_bytes(raw, byteorder="little", signed=True) / pow(10, decimals), decimals)
+        return int(val) if decimals == 0 else val
+
+    def toUnsignedInt(self, raw, decimals=0):
+        val = round(int.from_bytes(raw, byteorder="little", signed=False) / pow(10, decimals), decimals)
+        return int(val) if decimals == 0 else val
+
+
+class HouseCurrent(Packet):
+    def __str__(self):
+        return "{0}  {1:.1f} A house current".format(super().__str__(),
+            self.toSignedInt(self.frame[6:8], 1))
+
+class VehicleVoltage(Packet):
+    def __str__(self):
+        return "{0}  {1:.1f} V vehicle voltage".format(super().__str__(),
+            self.toUnsignedInt(self.frame[6:8], 2))
+
+class HouseVoltage(Packet):
+    def __str__(self):
+        return "{0}  {1:.1f} V house voltage".format(super().__str__(),
+            self.toUnsignedInt(self.frame[6:8], 2))
+
+class HouseCapacityAmpHours(Packet):
+    def __str__(self):
+        return "{0}  {1} Ah house capacity".format(super().__str__(),
+            self.toUnsignedInt(self.frame[6:8]))
+
+class HouseCapacityPercent(Packet):
+    def __str__(self):
+        return "{0}  {1} % house capacity".format(super().__str__(),
+            self.toUnsignedInt(self.frame[6:7]))
+
+class FreshPercent(Packet):
+    def __str__(self):
+        return "{0}  {1} % fresh water".format(super().__str__(),
+            self.toUnsignedInt(self.frame[6:8]))
+
+class GrayPercent(Packet):
+    def __str__(self):
+        return "{0}  {1} % gray water".format(super().__str__(),
+            self.toUnsignedInt(self.frame[6:8]))
+
+
+def parse_packet(frame):
+    if frame[0:3] == b"\xaa\x62\xf4":
+        if frame[3] == 0x0c:
+            if frame[5] == 0x02:
+                return HouseCurrent(frame)
+            elif frame[5] == 0x03:
+                return HouseVoltage(frame)
+            elif frame[5] == 0x05:
+                return HouseCapacityAmpHours(frame)
+            elif frame[5] == 0x06:
+                return HouseCapacityPercent(frame)
+        elif frame[3] == 0x14 and frame[5] == 0x02:
+            return FreshPercent(frame)
+        elif frame[3] == 0x18 and frame[5] == 0x02:
+            return GrayPercent(frame)
+        elif frame[3] == 0x44 and frame[5] == 0x03:
+            return VehicleVoltage(frame)
+    return Packet(frame)
+
 class Reader:
 
     def __init__(self, port):
@@ -33,16 +105,14 @@ class Reader:
         self.buffer(contents[-1])
         contents = contents[:-1]
         # Return the packet.
-        return bytes([start_byte]) + contents
-
-    def format_packet(self, packet):
-        return " ".join("{:02x}".format(byte) for byte in packet)
+        packet = parse_packet(bytes([start_byte]) + contents)
+        return packet
 
     def dump(self):
         start = time.time()
         while True:
             packet_start = time.time()
-            packet = self.format_packet(self.read_packet())
+            packet = self.read_packet()
             print("{0:11.6f}  {1}".format(packet_start - start, packet), flush=True)
 
     def help_understand(self):
